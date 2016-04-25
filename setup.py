4f047a7a28c83159751eae8d8524964dd37526e3
Will Barton <william.barton@cfpb.gov>
2016-04-25 07:54:09 -0400

    Make sure to include haystack search index in wheels

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(
     name = "regcore",
     version = "1.2.0", 
     license = "public domain", 
-    packages = find_packages()
+    packages = find_packages(),
+    include_package_data=True,
+    package_data = {
+        'regcore': ['templates/search/indexes/regcore/regulation_text.txt'],
+    }
 )
