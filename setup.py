2f375661bf1c96fdd031f2db218625681f55f38e
Will Barton <william.barton@cfpb.gov>
2016-04-25 07:56:17 -0400

    Clean up the requirements

--- setup.py
+++ setup.py
@@ -8,5 +8,16 @@ setup(
     include_package_data=True,
     package_data = {
         'regcore': ['templates/search/indexes/regcore/regulation_text.txt'],
-    }
+    },
+    install_requires=[
+        'django',
+        'django-haystack',
+        'jsonschema',
+        'pyelasticsearch',
+        'pysolr',
+    ],
+    classifiers=[
+        'License :: Public Domain',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication'
+    ]
 )
